3b1277a3d5ccc85ad8ec937477cf1c21bde58f1d
idleberg <git@idleberg.com>
2020-02-02 12:23:30 +0100

    yarn build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,7 @@
+/**
+ * Installs and optionally enables package dependencies
+ * @param {string} packageName
+ * @param {Object} options
+ */
 declare function satisfyDependencies(packageName: string, userOptions?: PackageOptions): Promise<void>;
 export { satisfyDependencies };
